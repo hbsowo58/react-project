9d2c47f4c1a2b282cd852c9b2b18241e83354c67
hbs <hbsowo58@gmail.com>
2020-06-22 10:52:10 +0900

    feat:add React-News-Viewer

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Game from './components/Game';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Game />
+//     </div>
+//   );
+// }
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Game />
+      </div>
+    );
+  }
+}
+
+// export default App;
